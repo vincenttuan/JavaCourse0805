9aa71bdca58963b9fabdb592d7ed1df69ede9a10
teacher <teacher@192.168.2.14>
2019-11-18 20:06:59 +0800

    Java 8

--- src/day26_db/CRUD.java
+++ src/day26_db/CRUD.java
@@ -18,7 +18,9 @@ public class CRUD {
     }
     
     public static void main(String[] args) {
-        insert(10, "Jack");
+        //insert(10, "Jack");
+        //update(3, 3, "three");
+        delete(3);
     }
     
     public static void insert(int age, String name) {
@@ -32,6 +34,37 @@ public class CRUD {
                 System.out.println("Insert Error");
             }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void update(int id, int age, String name) {
+        String sql = "Update student Set age=?, name=? Where id = ?";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, age);
+            pstmt.setString(2, name);
+            pstmt.setInt(3, id);
+            if(pstmt.executeUpdate() > 0) {
+                System.out.println("Update OK");
+            } else {
+                System.out.println("Update Error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void delete(int id) {
+        String sql = "Delete From student Where id = ?";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, id);
+            if(pstmt.executeUpdate() > 0) {
+                System.out.println("Delete OK");
+            } else {
+                System.out.println("Delete Error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     
