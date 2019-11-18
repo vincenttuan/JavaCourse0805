e689c75a6c3b28170d2352f69d3c168401868c03
teacher <teacher@192.168.2.14>
2019-11-18 20:59:22 +0800

    Java 8

--- test/day26_junit/SquareTest.java
+++ test/day26_junit/SquareTest.java
@@ -1,15 +1,30 @@
 package day26_junit;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class SquareTest {
+    
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("before class");
+    }
+    
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
     
     public SquareTest() {
     }
 
     @Test
     public void testArea() {
+        System.out.println("testArea");
         int excepted = 40;
         Square s = new Square(2);
         int actual = s.area();
@@ -18,10 +33,23 @@ public class SquareTest {
 
     @Test
     public void testArea2() {
+        System.out.println("testArea2");
         int excepted = 50;
         Square s = new Square(10);
         int actual = s.area2();
         assertEquals(excepted, actual);
+    }
+    
+    
+
+    @After
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("after class");
     }
     
 }
